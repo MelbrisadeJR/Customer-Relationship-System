d9efc6a0939e0c4500b9eca3c06d76c058201f0f
Lily Codebug <xianj.chen@qq.com>
2021-04-07 08:50:43 +0800

    format changed

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { GET_FEEDBACKS } from './types';
 
-const getFeedbacks = () => async dispatch => {
-    const res = await axios.get("http://localhost:8080//api/feedback/all");
-    dispatch({
-        type: GET_FEEDBACKS,
-        payload: res.data
-    });
-}
+const getFeedbacks = () => async (dispatch) => {
+  const res = await axios.get('http://localhost:8080//api/feedback/all');
+  dispatch({
+    type: GET_FEEDBACKS,
+    payload: res.data
+  });
+};
 
 export default getFeedbacks;
