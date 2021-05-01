4b0f04343f1ed09668d6be0afa4052c0351c0bbf
Tutuzz <73582134+Tutuzz@users.noreply.github.com>
2021-05-01 12:26:54 +1000

    Update Toolbar.js
    Add new product

--- src/views/product/ProductListView/Toolbar.js
+++ src/views/product/ProductListView/Toolbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {
@@ -12,6 +12,12 @@ import {
   makeStyles
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -25,6 +31,15 @@ const useStyles = makeStyles((theme) => ({
 
 const Toolbar = ({ className, ...rest }) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div
@@ -44,9 +59,96 @@ const Toolbar = ({ className, ...rest }) => {
         <Button
           color="primary"
           variant="contained"
+          onClick={handleClickOpen}
         >
           Add product
         </Button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">New Product</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please Add New Product
+            </DialogContentText>
+            <Button
+              variant="contained"
+              color="default"
+              className={classes.button}
+              fullWidth
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload Image
+            </Button>
+            <TextField required id="standard-required-sku" label="SKU" margin="normal" />
+            <TextField required id="standard-required-name" label="Name" margin="normal" />
+            <TextField
+              autoFocus
+              id="category"
+              label="Category"
+              type="category"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              autoFocus
+              id="brand"
+              label="Brand"
+              type="brand"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              autoFocus
+              id="manufacture"
+              label="Manufacture"
+              type="manufacture"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              required
+              id="selling_price"
+              label="Selling Price"
+            />
+            <TextField
+              required
+              id="cost_price"
+              label="Cost Price"
+            />
+            <TextField
+              autoFocus
+              id="discount"
+              label="Discount"
+              type="discount"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              autoFocus
+              id="stock"
+              label="Stock"
+              type="stock"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              id="discription"
+              label="Discription"
+              multiline
+              rows={4}
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Box>
       <Box mt={3}>
         <Card>
